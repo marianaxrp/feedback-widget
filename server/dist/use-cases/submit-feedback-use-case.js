ef387eeb01c30ce10494550b2019427857fa9ad4
Mariana Xavier <m.ribeiro.pinto@avanade.com>
2022-05-08 14:26:36 -0300

    chore: adjusts in backend for deploy

---
+++ server/dist/use-cases/submit-feedback-use-case.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SubmitFeedbackUseCase = void 0;
+class SubmitFeedbackUseCase {
+    constructor(feedbacksRepository, mailAdapter) {
+        this.feedbacksRepository = feedbacksRepository;
+        this.mailAdapter = mailAdapter;
+    }
+    async execute(request) {
+        const { type, comment, screenshot } = request;
+        if (!type) {
+            throw new Error("Type is required.");
+        }
+        if (!comment) {
+            throw new Error("Comment is required.");
+        }
+        if (screenshot && !screenshot.startsWith("data:image/png;base64")) {
+            throw new Error("Invalid screenshot format.");
+        }
+        await this.feedbacksRepository.create({
+            type,
+            comment,
+            screenshot
+        });
+        await this.mailAdapter.sendMail({
+            subject: "Novo feedback",
+            body: [
+                `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+                `<p>Tipo do feedback: ${type}`,
+                `<p>Comentário: ${comment}</p></div>`,
+                screenshot ? `<img src="${screenshot}" />` : ``
+            ].join("\n")
+        });
+    }
+}
+exports.SubmitFeedbackUseCase = SubmitFeedbackUseCase;
